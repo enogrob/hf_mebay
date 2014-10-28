613b53b49ce0a63ddcb8d3bc55080f40a135f4e7
Roberto Nogueira <enogrob@gmail.com>
2014-10-28 12:31:27 -0200

    Ad views working properly

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,5 +1,16 @@
 class AdsController < ApplicationController
-  before_action :set_ads, only: [:show]
+  before_action :set_ad, only: [:show]
+
   def show
+  end
+
+  def index
+    @ads = Ad.all
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ad
+    @ad = Ad.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :ads
-  root :to => 'ads#show'
+  root :to => 'ads#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
